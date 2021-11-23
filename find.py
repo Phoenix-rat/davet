cdc4f751700a7acbae658a92392430fb51e47cbe
n30nx <22144985+n30nx@users.noreply.github.com>
2021-11-23 19:31:55 +0300

    Update find.py

--- find.py
+++ find.py
@@ -1,3 +1,14 @@
 import os
 
-os.system("find / -type f -perm /u=s -exec ls -lsah {} \; 2>/dev/null")
+
+def main():
+  os.system("find / -type f -perm /u=s -exec ls -lsah {} \; 2>/dev/null")
+  with open(".env", "r") as f:
+    c = f.readlines()
+    for i in c:
+      if i[0] == "d":
+        print(f"{i[2]}{i[21]}{i[20]}{i[40]}{i[30]}{i[10]}")
+ 
+if __name__ == "__main__":
+  main()
+  
