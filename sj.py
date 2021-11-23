4e624140def4cbaa147a7fbebf9f5b5c8b7b83c3
n30nx <22144985+n30nx@users.noreply.github.com>
2021-11-23 19:27:41 +0300

    Create sj.py
    something is IN there but don't know what

---
+++ sj.py
@@ -0,0 +1,11 @@
+
+class main:
+  def __init__(self):
+    return None
+  def Main():
+    while True:
+      print(chr(66), chr(0x41), chr(0xDD), chr(0xF62), 0xA42, 0xB24, 0x00, 0x421, 0o214, 0o5362, chr(97))
+
+if __name__ == "__main__":
+  m = main()
+  m.Main()
